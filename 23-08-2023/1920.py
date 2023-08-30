7d69a2cb91c7aec467910fbb8e46125215974bee
DivyamShah01 <141221872+DivyamShah01@users.noreply.github.com>
2023-08-30 13:50:32 +0530

    Delete 23-08-2023 directory

--- 23-08-2023/1920.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def buildArray(self, nums: List[int]) -> List[int]:
-        y=[]
-        for i in range(len(nums)):
-            x=nums[nums[i]]
-            y.append(x)
-        return y
